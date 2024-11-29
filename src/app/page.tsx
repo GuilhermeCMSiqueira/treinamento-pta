8d20468b297eed4e1156fae6656a765c84d3d54b
Guilherme Siqueira <gcms2@cin.ufpe.br>
2024-11-28 22:05:54 -0300

    Site Treinamento PTA

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,155 @@
+"use client";
 import Image from "next/image";
+import { useState } from "react";
+import { CITiLogo, geralCITi } from "@/assets";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [isOpen, setIsOpen] = useState(false);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+  return (
+    <>
+      <div className="h-screen">
+        <nav className="bg-white h-36 shadow-md sticky w-full z-10 font-montserrat">
+          <div className="container mx-auto px-4 py-3 flex justify-between items-center">
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+              src={CITiLogo}
+              alt={"Logo do CITi"}
+              className="h-28 w-fit"
+            ></Image>
+            {/* Menu */}
+            <div className="hidden md:flex space-x-6">
+              <a href="#" className="text-gray-600 hover:text-gray-800">
+                Treinamento
+              </a>
+              <a href="#" className="text-gray-600 hover:text-gray-800">
+                PTA
+              </a>
+              <a href="#" className="text-gray-600 hover:text-gray-800">
+                Tailwind
+              </a>
+              <a href="#" className="text-gray-600 hover:text-gray-800">
+                CSS
+              </a>
+            </div>
+
+            <div className="md:hidden">
+              <button
+                onClick={() => setIsOpen(!isOpen)}
+                className="text-gray-600 hover:text-gray-800"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h16m-7 6h7"
+                  />
+                </svg>
+              </button>
+            </div>
+          </div>
+          {isOpen && (
+            <div className="bg-white shadow-md md:hidden">
+              <a
+                href="#"
+                className="block px-4 py-2 text-gray-600 hover:bg-gray-100"
+              >
+                Treinamento
+              </a>
+              <a
+                href="#"
+                className="block px-4 py-2 text-gray-600 hover:bg-gray-100"
+              >
+                PTA
+              </a>
+              <a
+                href="#"
+                className="block px-4 py-2 text-gray-600 hover:bg-gray-100"
+              >
+                Tailwind
+              </a>
+              <a
+                href="#"
+                className="block px-4 py-2 text-gray-600 hover:bg-gray-100"
+              >
+                CSS
+              </a>
+            </div>
+          )}
+        </nav>
+
+        <main className="flex h-[calc(100vh-9em)] font-montserrat">
+          <div className="w-full bg-gray-100 bg-center hidden md:flex justify-center">
+            <Image
+              src={geralCITi}
+              alt={"Todo mundo do CITi"}
+              className="w-fit"
+            ></Image>
+          </div>
+
+          <div className="w-full h-full bg-gray-100 flex justify-center items-center p-8 box-border">
+            <form className="bg-white shadow-lg rounded-lg p-8 w-full max-w-md border-[#51E678] border-2 flex flex-col flex-shrink">
+              <h2 className="text-2xl font-bold text-gray-800 mb-6">
+                Contate o CITi
+              </h2>
+              <div className="mb-4">
+                <label
+                  htmlFor="name"
+                  className="block text-gray-700 font-medium mb-2"
+                >
+                  Nome
+                </label>
+                <input
+                  type="text"
+                  id="name"
+                  className="w-full border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  placeholder="Seu Nome"
+                />
+              </div>
+              <div className="mb-4">
+                <label
+                  htmlFor="email"
+                  className="block text-gray-700 font-medium mb-2"
+                >
+                  Email
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  className="w-full border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  placeholder="Seu Email"
+                />
+              </div>
+              <div className="mb-4">
+                <label
+                  htmlFor="message"
+                  className="block text-gray-700 font-medium mb-2"
+                >
+                  Mensagem
+                </label>
+                <textarea
+                  id="message"
+                  className="w-full border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  placeholder="Sua Mensagem"
+                  rows="4"
+                ></textarea>
+              </div>
+              <button
+                type="submit"
+                className="w-full bg-[#51E678] text-white font-medium py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
+              >
+                Enviar Mensagem
+              </button>
+            </form>
+          </div>
+        </main>
+      </div>
+    </>
   );
 }
---
+++ src/assets/index.ts
@@ -0,0 +1,7 @@
+import CITiLogo from './citi.png';
+import geralCITi from './geralCiti.jpeg';
+
+export {
+    CITiLogo,
+    geralCITi,
+}
